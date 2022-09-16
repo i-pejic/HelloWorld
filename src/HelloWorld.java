88ab8a287dd10236a6b586f4d5ebc35469ec155b
i-pejic <s52456@edu.campus02.at>
2022-09-16 19:12:57 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    //Main Funktion automatisch erzeugen lassen
+    //main+TAB
+    //psvm+TAB
+    public static void main(String[] args) {
+        //Code für Ausgabe automatisch erzeugen lassen
+        //sout+TAB
+        System.out.println("Hello World!");
+        System.out.println("Welcome to KONF 2022!");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");
+    }
+}
